9e5b1689687bc6ddffe7d216980209f2985eb73b
Kyrill Tircher <kyrilltircher14@hotmail.com>
2017-11-23 00:51:35 +0100

    htaccess

--- controllers/Ajax.php
+++ controllers/Ajax.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 3600)) {
-	session_unset();    
+	session_unset();
 }
 
 if(isset($_POST['action']) && !empty($_POST['action'])) {
@@ -61,20 +61,20 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			return;
 	}
 }
-	
+
 
 	function changeCode($map){
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$code = $map['newCode'];
-		
+
 		$codedb = crypt($code, "JeSuisUnSalt");
-		
+
 		$stmt = prepareQuery('UPDATE blogCode SET code = ? WHERE id = 1', $db);
-		
+
 		$stmt->bindParam(1, $codedb, PDO::PARAM_STR);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -82,15 +82,14 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally {
 			$db = null;
 		}
-		
+
 		return 1;
-		
+
 	}
-	
+
 	function getDb(){
 		try{
 			$db = new PDO('mysql:host=u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=id0hod5qtqrkfrn0','lv17207rcng54wdp','pg990b34i44xktr9');
-			//$db = new PDO('mysql:host=tircherbxaktirch.mysql.db;dbname=tircherbxaktirch','tircherbxaktirch','1405Shakti');
 			$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 		}
@@ -99,20 +98,20 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		}
 		return $db;
 	}
-	
+
 	function prepareQuery($query, $db){
-		
+
 		$stmt = $db->prepare($query);
-	
+
 		return $stmt;
 	}
 
 	function chargerDownloadFile($code){
 		if(($db = getDb()) == 2)
 			return $db;
-			
+
 		$stmt = prepareQuery('SELECT * FROM blogCode WHERE id = 1', $db);
-			
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -120,7 +119,7 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally {
 			$db = null;
 		}
-		
+
 		$tab = $stmt->fetchAll();
 		if(sizeof($tab)==0){
 			return 7;
@@ -130,47 +129,47 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			}
 			return 7;
 		}
-		
-		
+
+
 	}
-	
+
 	function sentMessage($map){
 		$nom = $map['nomContact'];
 		$email = $map['mailContact'];
 		$message = $map['messageContact'];
-		
+
 		$texte = "Received from : " . $nom . "\n";
 		$texte .= "Mail of contact : " . $email . "\n";
 		$texte .= "Message left : \n\n" . $message;
-		
+
 		$longueur = strlen($texte);
-		
+
 		if($longueur > 1000){
 			return 5;
 		}
-		
+
 		if(mail('brigittevanatoru@hotmail.com', "Message venant d'un utilisateur du site",$texte))
 			return 1;
 		return 0;
 	}
-	
+
 	function commitModifyPost($map){
 		if(($db = getDb()) == 2)
 			return $db;
-			
+
 			$db->beginTransaction();
-			
+
 			$id = $map['idPoste'];
 			$titre = $map['titreBlog'];
 			$texte = $map['texte'];
 			$prioritaire = $map['prioritaire'];
 			$subtitle = $map['sousTitreBlog'];
 			$pinned;
-			
+
 			if(strcmp($prioritaire,"none") == 0){
 				$pinned = 0;
 			} else $pinned = 1;
-			
+
 			if(isset($_FILES['file'])){
 				$target_file = "../files/" . basename($_FILES['file']['name']);
 				$stmt = prepareQuery('UPDATE blogs SET title = ?, text = ?, pinned = ?, subtitle = ?, fichier = ? WHERE id = ?', $db);
@@ -180,35 +179,35 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 				$stmt = prepareQuery('UPDATE blogs SET title = ?, text = ?, pinned = ?, subtitle = ? WHERE id = ?', $db);
 				$stmt->bindParam(5, $id, PDO::PARAM_INT);
 			}
-			
+
 			$stmt->bindParam(1, $titre, PDO::PARAM_STR);
 			$stmt->bindParam(2, $texte, PDO::PARAM_STR);
 			$stmt->bindParam(3, $pinned, PDO::PARAM_BOOL);
 			$stmt->bindParam(4, $subtitle, PDO::PARAM_STR);
-			
+
 			try{
 				$stmt->execute();
 			} catch (PDOException $e){
 				$db = null;
 				return 2;
 			}
-			
+
 			if(!isset($_FILES['file'])){
 				$db->commit();
 				$db = null;
 				return 1;
 			}
-			
+
 			if($_FILES['file']['size'] > 1000000){
 				$db->rollback();
 				$db = null;
 				return 4;
 			}
-			
+
 			if(file_exists($target_file)){
 				unlink($targer_file);
 			}
-			
+
 			if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
 				$db->commit();
 				$db = null;
@@ -218,29 +217,29 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 				$db = null;
 				return 0;
 			}
-			
+
 	}
-	
+
 	function connexion($map){
 		if(($db = getDb()) == 2)
 			return $db;
-			
+
 		$stmt = prepareQuery('SELECT * FROM admins WHERE nick = ?', $db);
-		
+
 		$nom = $map['username'];
 		$password = $map['password'];
-		
+
 		$stmt->bindParam(1, $nom, PDO::PARAM_STR);
-		
+
 		try{
-			$stmt->execute(); 
+			$stmt->execute();
 		} catch (PDOException $e){
 			return 0;
 		} finally {
 			$db = null;
 		}
-		
-		
+
+
 		$tab = $stmt->fetchAll();
 		if(sizeof($tab)==0){
 			return 6;
@@ -252,18 +251,18 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			return 6;
 		}
 	}
-	
+
 	function commitPost($map){
 		if(($db = getDb()) == 2)
-			return $db;	
-			
+			return $db;
+
 		$db->beginTransaction();
-			
-		
+
+
 		$titre = $map['titreBlog'];
 		$texte = $map['texte'];
 		$subtitle = $map['sousTitreBlog'];
-		
+
 		if(isset($_FILES['file'])){
 			$target_file = "../files/" . basename($_FILES['file']['name']);
 			$stmt = prepareQuery('INSERT INTO blogs VALUES (DEFAULT, DEFAULT, ?, ?, ?, ?, DEFAULT, DEFAULT)', $db);
@@ -271,34 +270,34 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} else {
 			$stmt = prepareQuery('INSERT INTO blogs VALUES (DEFAULT, DEFAULT, ?, ?, ?, DEFAULT, DEFAULT, DEFAULT)', $db);
 		}
-		
+
 		$stmt->bindParam(1, $titre, PDO::PARAM_STR);
 		$stmt->bindParam(2, $subtitle, PDO::PARAM_STR);
 		$stmt->bindParam(3, $texte, PDO::PARAM_STR);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
 			$db = null;
 			return 2;
 		}
-				
+
 		if(!isset($_FILES['file'])){
 			$db->commit();
 			$db = null;
 			return 1;
 		}
- 
+
 		if($_FILES['file']['size'] > 10000000){
 			$db->rollback();
 			$db = null;
 			return 4;
 		}
-			
+
 		if(file_exists($target_file)){
 			unlink($targer_file);
 		}
-		
+
 		if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
 			$db->commit();
 			$db = null;
@@ -310,13 +309,13 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		}
 
 	}
-	
+
 	function chargerAccueil(){
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$stmt = prepareQuery('SELECT * FROM blogs WHERE blogs.deleted = FALSE ORDER BY blogs.pinned, blogs.date DESC', $db);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -324,7 +323,7 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally{
 			$db = null;
 		}
-		
+
 		$tab = $stmt->fetchAll();
 		if(sizeof($tab)==0){
 			return 0;
@@ -332,15 +331,15 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			return json_encode($tab);
 		}
 	}
-	
+
 	function deletePost($id){
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$stmt = prepareQuery('UPDATE blogs SET deleted=TRUE WHERE id = ?', $db);
-		
+
 		$stmt->bindParam(1, $id, PDO::PARAM_INT);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -348,18 +347,18 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally {
 			$db = null;
 		}
-		
+
 		return 1;
 	}
-	
+
 	function chargerPost($id){
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$stmt = prepareQuery('SELECT * FROM blogs WHERE blogs.id = ? AND blogs.deleted = 0', $db);
-		
+
 		$stmt->bindParam(1, $id, PDO::PARAM_INT);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -367,7 +366,7 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally {
 			$db = null;
 		}
-		
+
 		$tab = $stmt->fetchAll();
 		if(sizeof($tab)==0){
 			return 0;
@@ -375,13 +374,13 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			return json_encode($tab[0]);
 		}
 	}
-	
+
 	function chargerAbout(){
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$stmt = prepareQuery('SELECT * FROM aPropos WHERE id = 1', $db);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -389,28 +388,28 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 		} finally {
 			$db = null;
 		}
-		
+
 		$tab = $stmt->fetchAll();
-		
+
 		if(sizeof($tab)==0){
 			return 0;
 		} else {
 			return json_encode($tab[0]);
 		}
-		
+
 	}
-	
+
 	function changeAbout($map){
-		
+
 		if(($db = getDb()) == 2)
 			return $db;
-		
+
 		$stmt = prepareQuery('UPDATE aPropos SET texte = ? WHERE aPropos.id = 1', $db);
-		
+
 		$texte = $map['texte'];
-				
+
 		$stmt->bindParam(1, $texte, PDO::PARAM_STR);
-		
+
 		try{
 			$stmt->execute();
 		} catch (PDOException $e){
@@ -419,14 +418,14 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
 			$db = null;
 		}
 		return 1;
-			
+
 	}
-	
+
 	function deconnexion(){
 		session_unset();
 		return 1;
 	}
-	
+
 	function estConnecte(){
 		if(isset($_SESSION['name'])){
 			return true;
